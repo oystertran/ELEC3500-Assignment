12c21bf0f50afea527ba8f2ab39b931f83aed4c1
oystertran <quanghao81111@gmail.com>
2024-09-28 16:51:32 +1000

    Implemented barebone functions

--- ELEC3500_Assignment.py
+++ ELEC3500_Assignment.py
@@ -1,5 +1,8 @@
 from socket import *
 import os
+import time # import module to get last modification time of file
+from email.utils import formatdate
+
 
 # Global variables
 proxy_host='localhost'
@@ -14,10 +17,11 @@ def start_proxy_server(host, port):
     # Create a server socket
     servSock = socket(AF_INET, SOCK_STREAM)
     # TODO: Start - Bind the socket to a port and begin listening
-    socket.bind()
+    servSock.bind(host, port)
+    servSock.listen(10)
     # TODO: End
 
-    print('Proxy server running...')
+    print('Proxy server running...') # message to know that the socket is listening
 
     while True:
         cliSock, addr = servSock.accept()
@@ -27,7 +31,7 @@ def start_proxy_server(host, port):
 
 def handle_client(cliSock):
     # TODO: Start - Receive the request
-    request = 
+    request = cliSock.recv(BUFFER_SIZE).decode(utf-8)
     # TODO: End
 
     if not request:
@@ -47,14 +51,16 @@ def handle_client(cliSock):
         
     try:
         # TODO: Start - Create socket to fetch content from web
-        webSock = 
+        webSock = socket(AF_INET, SOCK_STREAM)
         # TODO: End
 
         # TODO: Start - From the url, get the host name and path
         # e.g the hostname is gaia.cs.umass.edu, 
         # the path is /wireshark-labs/HTTP-wireshark-file1.html 
-        webHostn = 
-        webPath = 
+        url = url.split("://", 1)[1] # take the second half of the url after https://
+        urlparts = url.split("/", 1) # split the hostname and path into 2 parts
+        webHostn = urlparts[0] # host name is the firt section
+        webPath = urlparts[1] # webpath is the second section
         # TODO: End
 
         # Replace empty path with '/' character
@@ -62,7 +68,7 @@ def handle_client(cliSock):
             webPath = '/'
         
         # TODO: Start - Establish connection with web server
-    
+        webSock.connect((webHostn, 80))
         # TODO: End 
 
         # Check whether the file exists in the cache
@@ -70,9 +76,9 @@ def handle_client(cliSock):
             print(f"File found in cache: {url}")
             # TODO: Start - Get the last modification time of the file and 
             # construct if-modified-since header to request
-            # (Hint: you may need to import another module to help with this)
-            lastModified = 
-            ifModSince = 
+            # (Hint: you may need to import another module to help with this)   
+            lastModified = os.path.getmtime(cacheFilePath) # get last modification time 
+            ifModSince = formatdate(timeval=lastModified, usegmt=True)
             #TODO: End
         else:
             ifModSince="\r\n"
@@ -81,20 +87,26 @@ def handle_client(cliSock):
         webRequest = "GET " + webPath + " HTTP/1.1\r\nHost: " + webHostn +"\r\n" + ifModSince 
 
         # TODO: Start - Send the request to the web server
-        
+        webSock.sendall(webRequest.encode('utf-8'))
         # TODO: End
 
         # TODO: Start - Receive response from the web server
-        response = 
+        response = ''
+        while True:
+            data = webSock.recv(BUFFER_SIZE)
+            if not data:
+                break
+            response += data
         # TODO: End
 
         # TODO: Check for 304 Not Modified status code
-        if :
+        if "304 Not Modified" in response:
         #TODO: End
-            
             print(f"Not Modified, sending file from cache: {url}")
             # TODO: Start - Read data from the cache and send to client
-        
+            with open(cacheFilePath, 'rb') as cache_file:
+                cached_data = cache_file.read()
+                cliSock.sendall(cached_data)
             # TODO: End
             
         else:
@@ -103,13 +115,13 @@ def handle_client(cliSock):
                 cache_file.write(response)
             
             # TODO: Start - Forward response to the client
-            
+            cliSock.sendall(response)
             # TODO: End
 
     # Handle errors
     except Exception as e:
         # TODO: Start - Print an error message and send HTTP error code to the client
-
+        print("ERROR CODE PRINTED");
         # TODO: End
 
     # TODO: Start - Close the client socket 
