efbb56f29324772fed7437e61218f1cd40da7241
oystertran <quanghao81111@gmail.com>
2024-10-12 01:50:08 +1100

    Report update and comment added

--- ELEC3500_Assignment.py
+++ ELEC3500_Assignment.py
@@ -32,7 +32,8 @@ def start_proxy_server(host, port):
 
 def handle_client(cliSock):
     # TODO: Start - Receive the request
-    request = cliSock.recv(BUFFER_SIZE).decode('utf-8')
+    request = cliSock.recv(BUFFER_SIZE).decode('utf-8') #The request is sent in utf-8 format so we decode it 
+    print ("Request Received") #Print to know we have received the request
     # TODO: End
 
     if not request:
@@ -41,6 +42,7 @@ def handle_client(cliSock):
     
     #Get the request url
     url = request.split()[1]
+    print("url is: "+url)
     
 
     # Remove unnecessary characters from url
@@ -50,20 +52,21 @@ def handle_client(cliSock):
     
     # Assign file path in cache
     cacheFilePath = os.path.join(CACHE_DIR, url.replace('/','_').replace(':','_'))
-        
+    print("cacheFilePath is: " + cacheFilePath)
     try:
         # TODO: Start - Create socket to fetch content from web
         webSock = socket(AF_INET, SOCK_STREAM)
+        print("Web Socket Created")
         # TODO: End
 
         # TODO: Start - From the url, get the host name and path
         # e.g the hostname is gaia.cs.umass.edu, 
         # the path is /wireshark-labs/HTTP-wireshark-file1.html
-        request_url = get_url(request)
-        parsed_url = urlparse(request_url)
-        url_parts = parsed_url.path.strip('/').split('/')
-        webHostn = url_parts[0] # host name is the firt section
-        webPath = "/" + url_parts[1] + "/" + url_parts[2] 
+        #Call helper function to get extract the host name and path from the url
+        webHostn = get_hostname(url)
+        print("web host is: " + webHostn)
+        webPath = get_path(url)
+        print("web path is: " + webPath)
         # TODO: End
 
         # Replace empty path with '/' character
@@ -82,22 +85,22 @@ def handle_client(cliSock):
             # construct if-modified-since header to request
             # (Hint: you may need to import another module to help with this)   
             lastModified = os.path.getmtime(cacheFilePath) # get last modification time 
-            ifModSince = formatdate(timeval=lastModified, usegmt=True)
             print("lastModified: " + str(lastModified))
+            ifModSince = formatdate(timeval=lastModified, usegmt=True)
             print("ifModSince: " + str(ifModSince))
             #TODO: End
         else:
             ifModSince="\r\n"
 
         # Build a simple GET request
-        webRequest = "GET " + webPath + " HTTP/1.1\r\nHost: " + webHostn +"\r\n" + ifModSince 
+        webRequest = f"GET {webPath} HTTP/1.1\r\nHost: {webHostn}\r\nIf-Modified-Since: {ifModSince}\r\n\r\n"
         print("webRequest: " + webRequest)
         # TODO: Start - Send the request to the web server
         webSock.sendall(webRequest.encode('utf-8'))
         # TODO: End
 
         # TODO: Start - Receive response from the web server
-        response = b''
+        response = b'' #Response is in byte form 
         while True:
             data = webSock.recv(BUFFER_SIZE)
             if not data:
@@ -107,7 +110,7 @@ def handle_client(cliSock):
         # TODO: End
 
         # TODO: Check for 304 Not Modified status code
-        if b"304 Not Modified" in response:
+        if b"304 Not Modified" in response: 
         #TODO: End
             print(f"Not Modified, sending file from cache: {url}")
             # TODO: Start - Read data from the cache and send to client
@@ -162,6 +165,35 @@ def get_url (request):
             referer_url = part.split(":", 1)[1].strip()
             return referer_url
     return None
+
+# Helper function that extract the host name from the url
+# Precondition: a url in the form of string existed
+# Postcondition: return the host name from the url
+def get_hostname(url):
+    # Remove leading slashes if present
+    url = url.lstrip('/')
+    
+    # Split by the first slash to separate hostname from the path
+    hostname = url.split('/', 1)[0]
+    
+    return hostname
+
+# Helper function that extract the path from the url
+# Precondition: a url in the form of string existed
+# Postcondition: return the path from the url
+def get_path(url):
+    # Remove leading slashes if present
+    url = url.lstrip('/')
+    
+    # Split by the first slash and extract the part after the hostname
+    parts = url.split('/', 1)
+    
+    # If there's a path after the hostname, return it; otherwise, return "/"
+    if len(parts) > 1:
+        return '/' + parts[1]
+    else:
+        return '/'
+
 
 if __name__ == "__main__":
     start_proxy_server(proxy_host, proxy_port)
