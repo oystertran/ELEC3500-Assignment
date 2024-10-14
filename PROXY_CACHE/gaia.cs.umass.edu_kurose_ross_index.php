e7b72799f0aa7d894df724f1cf0e3ed3a5cb3359
oystertran <quanghao81111@gmail.com>
2024-10-15 02:14:06 +1100

    Report

---
+++ PROXY_CACHE/gaia.cs.umass.edu_kurose_ross_index.php
@@ -0,0 +1,112 @@
+HTTP/1.1 200 OK
+Date: Sun, 13 Oct 2024 14:34:07 GMT
+Server: Apache/2.4.6 (CentOS) OpenSSL/1.0.2k-fips PHP/7.4.33 mod_perl/2.0.11 Perl/v5.16.3
+X-Powered-By: PHP/7.4.33
+Content-Length: 5920
+Content-Type: text/html; charset=UTF-8
+
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Computer Networking: a Top Down Approach</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/lux/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="custom.css" rel="stylesheet" type="text/css"/>
+	  
+	 
+	  
+  </head>
+  <body>
+    <!-- Required scripts for bootstrap to function -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+    <!-- This is the navbar -->
+    	<div id='navbar' class="container fixed-top">
+		<nav class="navbar navbar-expand-lg mx-auto mt-2 navbar-light py-2 bg-danger">
+			<button class="navbar-toggler mb-1" type="button" data-toggle="collapse" data-target="#navbar-toggle" aria-label="toggle">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbar-toggle">
+				<ul class="navbar-nav w-100">
+					<li class="nav-item">
+						<a href="index.php" class="btn btn-outline-primary mt-1 mb-1 ml-2">Home</a>
+					</li>
+					<li class="nav-item">
+						<a href="about.php" class="btn btn-outline-primary mt-1 mb-1 ml-2">About</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a href="#" id='navbarDropdown1' role='button' class="nav-link btn btn-outline-primary dropdown-toggle mt-1 mb-1 ml-2" data-toggle='dropdown'>
+							Resources (for everyone)
+						</a>
+						<div class='dropdown-menu' aria-labelledby='navbarDropdown1'>
+							<a class='dropdown-item' href='lectures.php'>Online Lectures</a>
+							<a class='dropdown-item' href='ppt.php'>Powerpoint</a>
+							<a class='dropdown-item' href='wireshark.php'>Wireshark Labs</a>
+							<a class='dropdown-item' href='knowledgechecks/'>Knowledge Checks</a>
+							<a class='dropdown-item' href='interactive/'>Interactive End-of-Chapter Problems</a>
+							<a class='dropdown-item' href='animations.php'>Interactive Animations</a>
+							
+						</div>
+					</li>
+					<li class="nav-item">
+						<a href="instructor.php" class="btn btn-outline-primary mt-1 mb-1 ml-2">Instructor Resources</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a href="#" id='navbarDropdown2' role='button' class="nav-link btn btn-outline-primary dropdown-toggle mt-1 mb-1 ml-2" data-toggle='dropdown'>
+							More
+						</a>
+						<div class='dropdown-menu' aria-labelledby='navbarDropdown2'>
+							<a class='dropdown-item' href='authors.php'>Authors</a>
+							<a class='dropdown-item' href='contact.php'>Contact</a>
+							<a class='dropdown-item' href='publisher.php'>Publisher's Page</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<img src='header_graphic_book_8E_3.jpg' class='img-fluid'/>
+	</div>
+	<div class='d-none d-lg-block'>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+	</div>
+	<div class='d-block d-lg-none'>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+	</div>    <!-- All content / each problem goes in here -->
+    <div class="container jumbotron" id="maincontainer">
+
+<p class='mt-n3'><span class='font-weight-bold'><em>Welcome</em></span> to the authors' website for the textbook,Computer Networking: a Top Down Approach (Pearson). The 8th edition of our textbook has been published in the spring of 2020 - find out <a href="eighth.php"> what's new in the 8th edition</a>. From this page here (check out the menu at the top of the page), you can find resources and information of interest to students, teachers, and readers alike. </p>
+<p>Since the publication of the first edition 21 years ago, the book has been adopted at many hundreds of colleges and universities, translated into 14 languages, and used by literally millions students and practitioners worldwide. We&rsquo;ve been overwhelmed by the positive response.</p>
+<p> This textbook is for a first course on computer networking. It has been used in computer science and electrical engineering departments, information systems and informatics departments, in business schools, and elsewhere - at both the undergraduate and graduate levels. It should also be of interest to practitioners in industry as well. Find out more about the textbook <a href="about.php">here</a>.</p>
+<p> You can't buy a hard copy of the 8th edition, but instead can rent (and then choose/pay to keep the hardcopy if you want a hard copy book). You can <a href="https://www.pearson.com/en-us/subject-catalog/p/computer-networking/P200000003334/9780136681557"> rent a copy or subscribe to Pearson+</a> from our publisher, or <a href="https://smile.amazon.com/Computer-Networking-2-downloads-James-Kurose-ebook/dp/B08KF4RQ5N/?_encoding=UTF8&pd_rd_w=KFlJH&content-id=amzn1.sym.a5eaa569-8a45-4530-84d2-2dcf8023272a&pf_rd_p=a5eaa569-8a45-4530-84d2-2dcf8023272a&pf_rd_r=J4Y11ZX9Y67WT5CYJE0C&pd_rd_wg=sjJtp&pd_rd_r=429b587a-1fb2-4a78-8632-462244442e65&ref_=pd_gw_ci_mcx_mi"> rent a hard copy or purchase a Kindle version</a> from Amazon, or <a href="https://www.vitalsource.com/products/computer-networking-james-kurose-keith-ross-v9780135928523"> rent a hard copy from VitalSource</a>. The ISBNs are:
+Print rental:  9780136681557,
+Pearson+ access: 9780135928615.</p>
+
+
+
+    <!-- End of the main container that holds the problem -->
+    </div>
+    <!-- This container holds the copyright and contact info -->
+    <div class="container">
+      <p>We gratefully acknowledge the programming and problem design work of John Broderick (UMass '21), which has really helped to substantially improve this site.</p>
+      <p> Copyright © 2010-2024 J.F. Kurose, K.W. Ross<br/>Comments welcome and appreciated: kurose@cs.umass.edu<br></p>
+    </div>
+  </body>
+</html>
---
+++ PROXY_CACHE/gaia.cs.umass.edu_kurose_ross_script.js
@@ -0,0 +1,37 @@
+HTTP/1.1 200 OK
+Date: Sun, 13 Oct 2024 14:34:19 GMT
+Server: Apache/2.4.6 (CentOS) OpenSSL/1.0.2k-fips PHP/7.4.33 mod_perl/2.0.11 Perl/v5.16.3
+Last-Modified: Mon, 19 Jul 2021 15:35:17 GMT
+ETag: "3aa-5c77baf269740"
+Accept-Ranges: bytes
+Content-Length: 938
+Content-Type: application/javascript
+
+// Set navbar button active when linked
+$(document).ready(function() {
+	var path = $(location).attr('pathname').split('/').pop();
+	$('.nav-item a').each(function() {
+		if($(this).attr('href') == path) {
+			$(this).parent().addClass('active');
+		}
+		else if(path == '' && $(this).attr('href') == 'index.php') { // index page
+			$(this).parent().addClass('active');
+		}
+	});
+	$('.dropdown-item').each(function() {
+		if($(this).attr('href') == path) {
+			$(this).parent().parent().addClass('active');
+		}
+	});
+});
+
+// Makes dropdowns maintain their color-scheme when a menu-entry is clicked
+$(document).on('focus', '.dropdown-item', function() {
+	$(this).parent().prev().css('color', '#FFFFFF');
+	$(this).parent().prev().css('border-color', '#FFFFFF');
+	$(this).parent().prev().css('background-color', '#840E0E');
+});
+
+$(document).on('mouseup', '.dropdown-item', function() {
+	$(this).parent().prev().css('background-color', '#9c1207');
+});
